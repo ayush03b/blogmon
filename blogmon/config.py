575833807855cbfde98daad012c6ef7992f4617a
ayusxhhb <its2003ayush@email.com>
2024-10-25 18:41:28 +0530

    little changes

--- blogmon/config.py
+++ blogmon/config.py
@@ -1,12 +1,14 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
-    MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_PORT = int(os.getenv('MAIL_PORT'))
+    SECRET_KEY = os.getenv('SECRET_KEY', 'e11c832c6aff3df7c9f583eca450eab6')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', 'False')
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') == 'True'
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'its2003ayush@gmail.com')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'svjc vlzd pflv jrcx')
     MAIL_USE_SSL = os.getenv('MAIL_USE_SSL') == 'True'
+    FLASK_APP = os.getenv('FLASK_APP', 'blogmon')
